f14234a6cb93275cf594462770c3914839066cfe
Admin <roman.mavlonov@gmail.com>
2020-04-08 15:26:35 +1000

    MODULES and PACKAGES are learned

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-weight = int(input("Weight: "))
-unit = input("(L)bs of (K)g? ")
-if unit.upper() == "L":
-    result = 0.4535929094356397 * weight
-    print(f"{result} kg")
-elif unit.upper() == "K":
-    result = weight / 0.4535929094356397
-    print(f"{result} lbs")
+from ecommerce.shipping import calc_shipping
 
+calc_shipping()
---
+++ converters.py
@@ -0,0 +1,8 @@
+
+def lbs_to_kgs(lbs):
+    print(lbs * 0.4543)
+
+
+def kgs_to_lbs(kgs):
+    print(kgs / 0.4543)
+
